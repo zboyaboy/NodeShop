23fe3ba18cb4d0642a8757d942354a260c389dda
unknown <zboya@msn.com>
2023-10-15 15:26:49 +0800

    3-项目的基本优化

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const Koa = require('koa')
+const { APP_PORT } = require('./config/config.default')
 const app = new Koa()
 
 app.use((ctx, next) => {
     ctx.body = 'hello!'
 })
 
-app.listen(3000, () => {
+app.listen(APP_PORT, () => {
     console.log('server is running on http://locahost:3000')
 })
